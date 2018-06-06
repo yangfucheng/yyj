f72e6a65e249d703dccdf8f53ac2352aa93b6a18
陶佳琦 <1531290060@qq.com>
2018-06-07 00:25:52 +0800

    评论

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,8 +1,30 @@
 <template>
 	<div>
-		<ul v-infinite-scroll="loadMore" infinite-scroll-disabled="loading" infinite-scroll-distance="10">
+		<!-- <ul v-infinite-scroll="loadMore" infinite-scroll-disabled="loading" infinite-scroll-distance="10">
 			<li v-for="item in list">{{ item }}</li>
-		</ul>
+		</ul> -->
+        <ul class='comment_list'>
+            <li>
+                <img src='../../common/images/logo.png' class='logo'/>
+                <div class='comment_container'>
+                    <div>
+                        <p class='user'>用户1314</p>
+                        <p class="comment_first">听说我们可以</p>
+                    </div>
+                    <ul>
+                        <li><span>钩子</span>：就是看到好看</li>
+                        <li><span>钩子</span>：就是看到好看</li>
+                    </ul>
+                </div>
+            </li>
+            <li>
+                <img src='../../common/images/logo.png' class='logo'/>
+                <div><p class="user">用户1314</p><p>听说我们可以</p></div>
+            </li><li>
+                <img src='../../common/images/logo.png' class='logo'/>
+                <div><p class="user">用户1314</p><p>听说我们可以</p></div>
+            </li>
+        </ul>
 	</div>
 </template>
 
@@ -36,7 +58,35 @@ export default {
 </script>
 
 <style lang='scss' scoped>
-li{
-	height:100px;
+.comment_list{
+    line-height:1.5;
+    >li{
+        display:flex;
+        width:100%;
+        padding:0.3rem 0.5rem;
+        box-sizing:border-box;
+        .comment_container{
+            flex:1;
+        }
+        .logo{
+        width:0.6rem;
+        height:0.6rem;
+        border:1px solid #ccc;
+        border-radius:50%;
+        margin-right:0.2rem;
+        }
+        .user{
+            color:#999;
+        }
+        ul{
+            margin-top:0.2rem;
+            background:#eef1ec;
+            color:#888;
+            padding:0.2rem;
+            span{
+                color:#7c91bd;
+            }
+        }
+    }
 }
 </style>
